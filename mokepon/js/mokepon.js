c178cf9ee718508ad38b962b48fc2d84012d2ae9
Alexis Quintero <alesete789@gmail.com>
2022-08-25 10:24:21 -0300

    se agrego el codigo para seleccionar una mascota

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,7 +1,34 @@
-function seleccionarMascotaJugador(){
-    alert("SELECCIONASTE TU MASCOTA!!🐯")
+function iniciarJuego(){
+    let buttonMascotaJugador = document.getElementById("button-mascota")
+    buttonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)   
 }
 
+function seleccionarMascotaJugador(){
+    let inputHipodoge = document.getElementById('Hipodoge')
+    let inputCapipepo = document.getElementById('Capipepo')
+    let inputRatigueya = document.getElementById('Ratigueya')
+    let inputLangostelvis = document.getElementById('Langostelvis')
+    let inputTucapalma = document.getElementById('Tucapalma')
+    let inputPydos = document.getElementById('Pydos')
+    if( inputHipodoge.checked){
+        alert('seleccionaste a Hipodoge 💧🐶')
+    } else if(inputCapipepo.checked){
+        alert('seleccionaste a Capipepo')
+    }else if(inputRatigueya.checked){
+        alert('seleccionaste a Ratigueya ')
+    }else if(inputLangostelvis.checked){
+        alert('seleccionaste a Langostelvis')
+    }else if(inputTucapalma.checked){
+        alert('seleccionaste a Tucapalma')
+    }else if(inputPydos.checked){
+        alert('seleccionaste a Pydos')
+    }else{
+        alert('olvidaste elegir una mascota!')
+    }
 
-let buttonMascotaJugador = document.getElementById("button-mascota")
-buttonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+
+
+   
+}
+window.addEventListener("load", iniciarJuego )
